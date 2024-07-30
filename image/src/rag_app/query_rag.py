93f43afe6f4c63f8e52fb7a3e552dd45ae28887f
Deshant Singh Bani <135222639+DeshantBani@users.noreply.github.com>
2024-07-30 14:46:03 +0530

    Update query_rag.py

--- image/src/rag_app/query_rag.py
+++ image/src/rag_app/query_rag.py
@@ -44,7 +44,3 @@ def query_rag(query_text: str) -> QueryResponse:
     return QueryResponse(
         query_text=query_text, response_text=response_text, sources=sources
     )
-
-
-if __name__ == "__main__":
-    query_rag("How much does a landing page cost to develop?")
